c82c1140d830d9086843f179a96f790ea34bbde5
Andrew Fleming <andrewfleming@Andrews-MacBook-Air-2.local>
2017-07-20 09:42:47 +0100

    Initial commit

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by andrewfleming on 20/07/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String [] args) {
+        System.out.println("Hello, World!");
+    }
+}
